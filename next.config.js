4cea86163ea04577212557ada80db91274ffac86
gun7728 <gun7728@gmail.com>
2023-10-09 21:20:13 +0900

    feat : 기본 설정 및 메인 페이지 레이아웃 제작 (2023.10.09. 함건)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, {isServer}) => { 
+        // canvas 관련 설정 추가
+        config.module.rules.push({test : /\.node$/, use: 'raw-loader'})
+
+        if (!isServer) config.externals.push('canvas');
+        return config;
+    }
+}
 
 module.exports = nextConfig
