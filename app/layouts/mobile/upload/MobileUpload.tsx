949a1f8343194512231fb940c3191c22e401f5c0
gun7728 <gun7728@gmail.com>
2023-11-07 01:12:48 +0900

    feat : video upload 23.11.07. gun

--- app/layouts/mobile/upload/MobileUpload.tsx
+++ app/layouts/mobile/upload/MobileUpload.tsx
@@ -2,9 +2,49 @@ import styles from "./MobileUpload.module.scss"
 import {motion} from "framer-motion"
 import useViewStore from "@/app/store/view";
 import Image from "next/image";
-import {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useState} from "react";
+import useUserStore from "@/app/store/user";
+import useTokenStore from "@/app/store/token";
+import {anchoredResizeCoordinatesAlgorithm} from "advanced-cropper";
 
 export default function MobileUpload(){
+    const { info} = useUserStore()
+    const { accessToken } = useTokenStore()
+    const [title, setTitle] = useState<string>('')
+    const [content, setContent] = useState<string>('')
+    const [hashTag, setHashTag] = useState<string>('')
+    const [files, setFile] = useState<any>();
+    const uploadVideo = (e:any) =>{
+        let target: any = e.target
+        let files: any = e.target.files[0]
+        setFile(files)
+    }
+
+    const post = () =>{
+        const form = new FormData();
+        form.append("title", title);
+        form.append("hashTag", hashTag);
+        form.append("content", "");
+        form.append("description", content);
+        form.append("changeLanguage", "korean");
+        form.append("uidx", String(info.id));
+        form.append("vFile", files, "video");
+
+        fetch('/api/video/upload',{
+            body: form,
+            method:"POST",
+            headers: {
+                // "Content-Type": "multipart/form-data",
+                Authorization: `Bearer ${accessToken}`,
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            }})
+            .then((response) => response.json())//읽어온 데이터를 json으로 변환
+            .then((json) => {
+                console.log(json)
+            })
+            .catch((err) => {console.log(err)});
+    }
+
     const { setView } = useViewStore()
     const [imageUrl, setImageUrl] = useState<string>('');
 
@@ -43,8 +83,9 @@ export default function MobileUpload(){
                     <div style={{paddingBottom:'15px'}}>
                         <div style={{display:"flex"}}>
                             <div>
-                                <input className={styles.title_input} type="text" placeholder={"제목"}/>
-                                <textarea className={styles.contents_input} placeholder={"내용"}/>
+                                <input className={styles.title_input} type="text" placeholder={"제목"} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setTitle(e.target.value)}/>
+                                <textarea className={styles.contents_input} placeholder={"내용"}  onChange={(e:React.ChangeEvent<HTMLTextAreaElement>)=>setContent(e.target.value)}/>
+                                <input className={styles.title_input} type="text" placeholder={"해쉬태그"} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>setHashTag(e.target.value)}/>
                             </div>
                             <div className={styles.upload_thumb_wrap}>
                                 <label htmlFor="thumb">
@@ -70,7 +111,7 @@ export default function MobileUpload(){
                             <label htmlFor="file">
                                 <div className={styles.upload_video}><i className="fa-solid fa-video"></i> 동영상 업로드</div>
                             </label>
-                            <input type="file" name="file" style={{display:"none"}} id="file"  accept="video/*" />
+                            <input type="file" name="file" style={{display:"none"}} id="file"  accept="video/*"  onChange={(e)=>{uploadVideo(e)}}/>
                         </div>
                     </div>
                     <ul className={styles.upload_sub}>
@@ -87,6 +128,7 @@ export default function MobileUpload(){
                                번역 언어
                                <label className={styles.toggleControl}>
                                    <input type="checkbox" />
+                                   kr/en
                                    <span className={styles.control
 
                                    }></span>
@@ -121,6 +163,7 @@ export default function MobileUpload(){
                        </li>
 
                     </ul>
+                    <button onClick={()=>post()}>업로드</button>
                 </div>
             </div>
         </motion.div>
--- app/layouts/web/MainView.tsx
+++ app/layouts/web/MainView.tsx
@@ -1,14 +1,7 @@
 import React from "react";
-import TopNav from "@/app/layouts/web/header/TopNav";
-import SideNav from "@/app/layouts/web/side/SideNav";
 import './web.scss'
 import useViewStore from "@/app/store/view";
-import UploadView from "@/app/layouts/web/views/upload/UploadView";
 import useUserStore from "@/app/store/user";
-import LoginView from "@/app/layouts/web/views/login/LoginView";
-import ProfileView from "@/app/layouts/web/views/profile/ProfileView";
-import VideoView from "@/app/layouts/web/views/video/VideoView";
-
 export default function MainView({children}:{children:React.ReactNode}){
     const { curView } = useViewStore();
     const {loginForm} = useUserStore()
